1f27e9a8110859687268ff73971b3370ffb4800c
Stefan-Groen <stefangroen.gs@gmail.com>
2025-11-11 21:21:56 +0100

    updated sql to match current database

--- create-db.sql
+++ create-db.sql
@@ -1,21 +1,21 @@
 CREATE TABLE IF NOT EXISTS articles (
-  id                  SERIAL PRIMARY KEY,
-  status              TEXT DEFAULT 'PENDING',
-  title               TEXT NOT NULL,
-  link                TEXT NOT NULL UNIQUE,
-  summary             TEXT,
-  date_published      TIMESTAMPTZ,
-  source              TEXT,
-  date_added          TIMESTAMPTZ DEFAULT NOW(),
-  classification      TEXT DEFAULT '' CHECK (classification IN ('Threat','Opportunity','Neutral','Error: Unknown','')),
-  explanation         TEXT DEFAULT '',
-  reasoning           TEXT DEFAULT '',
-  classification_date TIMESTAMPTZ,
-  starred             BOOLEAN DEFAULT false NOT NULL
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    link TEXT NOT NULL UNIQUE,
+    date_published TIMESTAMP WITH TIME ZONE,
+    summary TEXT,
+    source TEXT,
+    date_added TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    status TEXT DEFAULT 'PENDING',
+    classification TEXT,
+    explanation TEXT,
+    advice TEXT,
+    reasoning TEXT,
+    classification_date TIMESTAMP WITH TIME ZONE,
+    starred BOOLEAN DEFAULT FALSE
 );
 
-CREATE INDEX IF NOT EXISTS idx_articles_date_published ON articles(date_published);
+CREATE INDEX IF NOT EXISTS idx_articles_link ON articles(link);
 CREATE INDEX IF NOT EXISTS idx_articles_status ON articles(status);
-CREATE INDEX IF NOT EXISTS idx_articles_classification ON articles(classification);
-CREATE INDEX IF NOT EXISTS idx_articles_classification_date ON articles(classification_date);
+CREATE INDEX IF NOT EXISTS idx_articles_date_published ON articles(date_published);
 CREATE INDEX IF NOT EXISTS idx_articles_starred ON articles(starred);
